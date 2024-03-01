3488e5cfb132672df6c499552f368f059bf3d262
abc <abc2@gmail.com>
2024-03-01 12:38:26 +0530

    commit file

--- src/main/java/com/example/apigatway/ApiGatewayApplication.java
+++ src/main/java/com/example/apigatway/ApiGatewayApplication.java
@@ -1,13 +1,20 @@
 package com.example.apigatway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
+    private static final Logger logger = LoggerFactory.getLogger(ApiGatewayApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+        logger.info("info testing");
+        logger.warn("warn testing");
+        logger.debug("debug testing");
+        logger.error("error testing");
     }
 
 }
